24279aa1f891e736a11b7bd7047a9391393d9a3d
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2020-05-24 15:42:44 +0200

    [FIX]: Fixed order date display

--- src/components/ui-elements/DashboardOrderTable.js
+++ src/components/ui-elements/DashboardOrderTable.js
@@ -17,15 +17,27 @@ function DashboardOrderTable({ user }) {
                 fontWeight: "bold",
               }}
             >
-              <td>{item[item.length - 1].split("T")[0]}</td>
+              <td
+                style={{
+                  width: "50%",
+                }}
+              >
+                Order Date:
+              </td>
               <td></td>
               <td></td>
-              <td></td>
+              <td
+                style={{
+                  width: "50%",
+                }}
+              >
+                {item[item.length - 1].split("T")[0]}
+              </td>
             </tr>
             {item.map((product, i) =>
               i !== item.length - 1 ? (
                 <tr>
-                  <td>{product.name}</td>
+                  <td>{product.name.substring(0, 10)}</td>
                   <td>${product.price}</td>
                   <td>x{product.quantity}</td>
                   <td style={{ textAlign: "right" }}>
