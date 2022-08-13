ac695a6902da4905945279b6839e45361fdb5aac
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2022-08-13 12:43:46 +0100

    Testing renewed token

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+// TEST PUSH
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
