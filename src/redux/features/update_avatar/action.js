a3ab7a1f9972450b2c6a1fe6c6c92b46d84b7445
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2022-12-02 09:37:19 +0000

    Fixed unused variables

--- src/redux/features/update_avatar/action.js
+++ src/redux/features/update_avatar/action.js
@@ -22,7 +22,7 @@ export const update_profile = (email, body) => async (dispatch) => {
   const token = localStorage.getItem('store_user_token');
   const filteredToken = token.replace(/"/g, '');
 
-  https: try {
+  try {
     const request = await axios.post(
       'https://clean-pumps-tick.cyclic.app/api/v1/user/updateAvatar/' + email,
       body,
