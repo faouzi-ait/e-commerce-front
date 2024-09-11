3d7e25964f5a31116d54d3c47af8bf89c060e18b
faouzi-ait <33617564+faouzi-ait@users.noreply.github.com>
2024-09-11 19:08:59 +0100

    Update action.js

--- src/redux/features/contact/action.js
+++ src/redux/features/contact/action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { localUrl, prodUrl } from '../../../utils/utilities';
+import { localUrl } from '../../../utils/utilities';
 import { SEND_MESSAGE_SUCCESS, SEND_MESSAGE_FAILURE } from '../../types';
 
 const contact_success = (message) => {
